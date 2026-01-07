8d9eb16d0e1ead0a0310a3b5892fc4f4b746f9d2
Prem A S <premas@Prems-MacBook-Air.local>
2026-01-07 11:07:45 +0530

    Update homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ export default function Home() {
     <div className="flex flex-col gap-3 flex-column min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-5xl font-semibold">Hello, I'm Prem 👋</h1>
       <p>Freelance web developer working for small business</p>
+      <p>+91 994081233
+      </p>
     </div>
   );
 }
